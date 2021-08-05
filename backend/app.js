3e53c4782542aa175745e56548e16401a3870cea
alwin <alwin24012002@gmail.com>
2021-08-05 18:05:19 +0530

    errors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+// const dotenv = require('dotenv');
 
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -9,6 +10,7 @@ const path = require('path');
 const errorMiddleware = require('./middlewares/errors');
 
 if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' });
+// dotenv.config({ path: 'backend/config/config.env' });
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,7 +29,7 @@ app.use('/api/v1', order);
 if (process.env.NODE_ENV === 'PRODUCTION') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.js'))
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
     })
 }
 
